819a5a71c996191b0cc0f2f9e7db047d7a76c0b3
yatomiyoshi <seno.yatomi.0407@gmail.com>
2023-06-23 20:26:34 +0900

    create ch2

--- ch1/resolve-and-reject.js
+++
@@ -1,7 +0,0 @@
-Promise.resolve(200);
-new Promise((resolve) => resolve(200));
-Promise.resolve(200).then((value) => console.log(value));
-
-Promise.reject(new Error("Error"));
-new Promise((resolve, reject) => reject(new Error("Error")));
-Promise.reject(new Error("Error")).catch((error) => console.log(error));
